d82c15a37dd96ee88e952f26ee193d160c8b61fa
Sachingiri619 <sachingiri619@gmail.com>
2021-05-03 20:49:00 +0545

    second

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Slider1 from './components/Slider1'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Slider1 />
+      </div>
+    );
+  }
+}
+
+export default App;
